03d58a39d1499595ae8ff80603e48e1302570e1c
Daktari <dindijjames@gmail.com>
2018-01-08 12:34:29 +0000

    Sort the migrations issue

---
+++ tests/test_bucketlist.py
@@ -0,0 +1,18 @@
+# # test_bucketlist.py
+
+# import unittest
+# import os
+# import json
+# from app import create_app, db
+
+# class BucketlistTestCase(unittest.TestCase):
+#     """Represents the Bucketlist test case"""
+    
+#     def setUp(self):
+#         """Define test variables and initialize app"""
+#         self.app = create_app(config_name="testing")
+#         self.client = self.app.test_client
+#         self.bucketlist = {'name': 'Go to Bahamas for honeymoon'}
+        
+#         # binds the app to the current context
+        
